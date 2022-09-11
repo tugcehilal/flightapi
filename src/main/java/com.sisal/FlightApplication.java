8c8fb65c82090c9659ea10916215dba13434ae4c
computer <computer@computers-MacBook-Pro.local>
2022-09-11 15:51:05 +0300

    test2

--- src/main/java/com.sisal/FlightApplication.java
+++ src/main/java/com.sisal/FlightApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlightApplication {
-  //test
+  //test2
     public static void main(String[] args) {
         SpringApplication.run(FlightApplication.class, args);
     }
